112fccf9f0b93b703559e10511c0d49d8fcdea95
Alessandro Mileto <alessandromileto22@gmail.com>
2021-03-28 13:19:19 +0200

    Aggiunta spiegazione di funzione global u nel pendolo. Rimossi file accessori

--- 19-03/main_pendolo.m
+++ 19-03/main_pendolo.m
@@ -2,37 +2,34 @@ clear;
 close all
 
 % intervallo di integrazione
-toss = [0 100]; 
+toss = [0 40]; 
 
 % passo di integrazione
 
-h = 0.05;
+h = 0.2;
 
 %condizioni iniziali 
-x0 = [1/2 0]; 
+x0 = [1/2 0]; %phi(0)=0.5 phi'(0)=0
 
 % Eulero Esplicito
 
-[t1,x_sol1] = eulero_esp(@pendolo,toss,x0,h);
-
+[t1,x_sol1] = eulero_esp(@pendolo,toss,x0,h); % condizioni di validità di Eulero non verificate -> il metodo diverge e quindi non è adatto al caso in esame (NON LINEARE) 
 
 % Eulero Implicito
 [t2,x_sol2] = eulero_imp(@pendolo,toss,x0,h);
 
 % Runge-Kutta ordine 2
-
 [t3,x_sol3] = rungekutta2(@pendolo,toss,x0,h);
 
 % Runge-Kutta ordine 4
-
 [t4,x_sol4] = rungekutta4(@pendolo,toss,x0,h);
 
 
 % ode45
+[t5,x_sol5] = ode45(@pendolo,toss,x0,odeset('InitialStep',h));
 
-%[t5,x_sol5] = ode45(@pendolo,toss,x0,odeset('InitialStep',h));
 
-
-plot(t1,x_sol1,'g--*',t2,x_sol2,'r-.d',t3,x_sol3,'k--o',t4,x_sol4,'c--x')%,t5,x_sol5,'b-')
+plot(t1,x_sol1(:,1),'g--*',t2,x_sol2(:,1),'r-.d',t3,x_sol3(:,1),'k--o',t4,x_sol4(:,1),'c--x',t5,x_sol5,'b-')
+title("Simulazione sistema dinamico: PENDOLO"); 
 grid
 legend('Eulero Esplicito','Eulero Implicito','Runge-Kutta 2','Runge-Kutta 4','Ode45')
--- 19-03/pendolo.m
+++ 19-03/pendolo.m
@@ -1,16 +1,25 @@
-function xpunto =  pendolo(t,x) % t è indipendente, x è indipendente 
-%pendolo senza forzamento esterno 
+function xpunto =  pendolo(t,x) % t è indipendente, x è dipendente -> come nel caso dell'equazione non lineare  
+%Funzione generatrice del pendolo (con forzamento aggiunto). La funzione
+%restituisce le derivate della funzione che descrive il pendolo phi(t).
+%Nello specifico xpunto=[phi', phi'']. 
+%PARAMETRI
 %l -> lunghezza asta
 %m -> massa puntiforme 
 %Fr -> coefficiente attrito 
 %g -> accelerazione gravità 
-l=0.5;
+%FORZAMENTO ESTERNO u(t) 
+%Ode e gli altri metodi di MATLAB non permettono di fornire u come
+%parametro e quindi definiamo il profilo temporale del forzamento
+%all'interno della funzione pendolo. 
+l=5;
 m=30;
 Fr=4;
-g=9.8;
+g=9.81;
+
 u=30*sin(2*pi*0.1*t); %lavora bene solo se l'ingresso è una formula chiusa --> se è una sequenza di campioni non funzionerebbe 
-x1p=x(1);  
-x2p=-(g/l)*sin(x(1))-(Fr/(m*l^2)*(2/pi)*atan(10*x(2)))-u/(m*l^2);  % è una soluzione antipatica perchè dovrei modificare la funzione pendolo 
+
+x1p=x(2); %derivata prima di phi(t) 
+x2p=-(g/l)*sin(x(1))-(Fr/(m*l^2))*(2/pi)*atan(10*x(2))-u/(m*l^2);  % è una soluzione antipatica perchè dovrei modificare la funzione pendolo 
 xpunto=[x1p;x2p];
 end 
 
